726e37a1b50374059fd62f5bb54b551f0c746c3a
RobdeMooij <45096223+RobdeMooij@users.noreply.github.com>
2019-05-26 01:30:07 +0200

    added comments and scaling

--- blend_to_json.py
+++ blend_to_json.py
@@ -1,20 +1,32 @@
 #exec(compile(open("C:/Users/s167917/Documents/#School/Jaar 3/4 Project USE Robots Everywhere/model/blend_to_json.py").read(), "C:/Users/s167917/Documents/#School/Jaar 3/4 Project USE Robots Everywhere/model/blend_to_json.py", 'exec'))
 import json
 import bpy
-data = {}
+
+# get object
 obj = bpy.context.active_object
+
+#define empty dictionary/arrays
+data = {}
 vertices = []
 triangles = []
 materials = []
 normals = []
+
+#loop over vertices and scale them
 for vertex in obj.data.vertices.values():
-    vertices.append((vertex.co.x,vertex.co.y,vertex.co.z))
+    vertices.append((vertex.co.x*10,vertex.co.y*10,vertex.co.z*10))
+    
+#loop over triangles and get indices+1 (for Matlab indexing), normals and material indices
 for triangle in obj.data.polygons:
     triangles.append((triangle.vertices[0]+1,triangle.vertices[1]+1,triangle.vertices[2]+1))
     normals.append(triangle.normal[:])
     materials.append(triangle.material_index+1)
+    
+#data into dictionary
 data["vertices"] = vertices
 data["triangles"] = triangles
 data["materials"] = materials
 data["normals"] = normals
-json.dump(data,open('iss.json','w'))
+
+#dictionary to file
+json.dump(data,open('test.json','w'))
