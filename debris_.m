8f3c649b801681ad893d0800d21039213518a531
RobdeMooij <45096223+RobdeMooij@users.noreply.github.com>
2019-06-06 23:26:19 +0200

    new ISS models and added debris start position

--- debris_.m
+++ debris_.m
@@ -14,20 +14,21 @@ classdef debris_  < handle
         function obj = debris_(azimuth,elevation,distance,diameter,dt)
             %initialize a debris object
             %input:
-            % position      start position
-            % diameter      diameter of sphere shaped debris
-            % density       debris material density
+            % azimuth       angle in the 
+            % elevation
+            % distance
+            % diameter
+            % dt
             %output:
             % obj           debris object with properties based on inputs
             
             obj.diameter  = diameter;
             obj.mass      = 61*(diameter/2)^2.26;
             
-            obj.direction = [0 1 0];%[sin(azimuth)*cos(elevation) cos(azimuth)*cos(elevation) sin(elevation)];
-            obj.position  = [0 0 6371e3+400e3];%+[rand()-0.5 rand()-0.5 rand()-0.5]*50;
+            obj.direction = [cos(azimuth)*cos(elevation) sin(azimuth)*cos(elevation) sin(elevation)];
+            obj.position  = [0 0 6.371e6+400e3]+[rand()-0.5 rand()-0.5 rand()-0.5]*50;
             obj.velocity  = 7700;
-            obj.get_start_position(distance,dt)            
-%             obj.energy    = obj.mass*obj.velocity^2/2;
+            obj.get_start_position(distance,dt)
         end
     end
     methods(Access = public)
@@ -48,31 +49,44 @@ classdef debris_  < handle
         function step(this,Fr,dt)
             Fg = -3.9857e+14*this.position/((this.position(1)^2+this.position(2)^2+this.position(3)^2)^1.5);
             this.acceleration = Fg+Fr/this.mass;
-            vel = this.velocity+this.acceleration.*dt;
+            vel = this.velocity*this.direction+this.acceleration.*dt;
             this.velocity = sqrt(vel(1)^2+vel(2)^2+vel(3)^2);
             this.direction = vel/this.velocity;
-            this.position = this.position+this.velocity.*dt;
+%             prev_position = this.position;
+            this.position = this.position+vel.*dt;
+%             line([prev_position(1),this.position(1)],[prev_position(2),this.position(2)],[prev_position(3),this.position(3)],'LineWidth',1,'Color','c')
         end
         function back_step(this,dt)
             this.acceleration = -3.9857e+14*this.position/((this.position(1)^2+this.position(2)^2+this.position(3)^2)^1.5);
-            vel = this.velocity+this.acceleration.*dt;
+            vel = this.velocity*this.direction+this.acceleration.*dt;
             this.velocity = sqrt(vel(1)^2+vel(2)^2+vel(3)^2);
             this.direction = vel/this.velocity;
-            prev_position = this.position;
-            this.position = this.position-vel.*dt;
-            disp(this.position)
-            line([prev_position(1),this.position(1)],[prev_position(2),this.position(2)],[prev_position(3),this.position(3)],'LineWidth',1,'Color','k')             
+%             prev_position = this.position;
+            this.position = this.position+vel.*dt;
+%             line([prev_position(1),this.position(1)],[prev_position(2),this.position(2)],[prev_position(3),this.position(3)],'LineWidth',2,'Color','k')
         end
     end
     methods(Access = protected)
         function get_start_position(this,distance,dt)
+%             plot3(this.position(1),this.position(2),this.position(3),'O','Color','k','MarkerSize',20,'MarkerFaceColor','g')
             dist = 0;
-            begin_position = this.position;
             while dist < distance
-                this.back_step(-dt)
-                dist = sqrt((begin_position(1)-this.position(1))^2+(begin_position(2)-this.position(2))^2+(begin_position(3)-this.position(3))^2);
+                prev_pos = this.position;
+                this.back_step(dt)
+                dist = dist+sqrt((prev_pos(1)-this.position(1))^2+(prev_pos(2)-this.position(2))^2+(prev_pos(3)-this.position(3))^2);
             end
+%             plot3(this.position(1),this.position(2),this.position(3),'O','Color','k','MarkerSize',10,'MarkerFaceColor','r')
+            this.direction = -this.direction;
         end
+%         function get_end_position(this,distance,dt)            
+%             dist = 0;
+%             while dist < distance
+%                 prev_pos = this.position;
+%                 this.step(0,dt)
+%                 dist = dist+sqrt((prev_pos(1)-this.position(1))^2+(prev_pos(2)-this.position(2))^2+(prev_pos(3)-this.position(3))^2);
+%             end
+%             plot3(this.position(1),this.position(2),this.position(3),'O','Color','k','MarkerSize',10,'MarkerFaceColor','b')            
+%         end
     end
 end
 
--- mesh_.m
+++ mesh_.m
@@ -1,26 +1,26 @@
 classdef mesh_ < handle
-   properties       
+   properties
+       position         %vector coordinates of position
        vertices         %vertices coordinates
        indices          %vertices indices that make up triangle
        triangles        %triangle_ objects
        materials        %triangles material indices
-%        normals          %normal vectors
    end
    methods(Access = public, Static)
         function obj = mesh_(file_name)
             %create mesh_ object based on (.json or .txt) file
-			json_file = fopen(file_name);
+            json_file = fopen(file_name);
             try %possible bug in jsondecode in Matlab version 2018b and higher
                 json_data = jsondecode(fread(json_file,inf,'*char'));
             catch %use JSONlab toolbox (has to be installed)
                 json_data = loadjson(file_name);
             end
             fclose(json_file);
-            obj.vertices    = json_data.vertices;
-            obj.indices     = json_data.triangles;
+            obj.position    = [0 0 6371e3+400e3];
+            obj.vertices    = json_data.vertices+obj.position;
+            obj.indices     = json_data.indices;
             obj.materials   = json_data.materials;
-%             obj.normals     = json_data.normals;
-
+            
             %create triangle_ objects
             for i = 1:size(obj.indices,1)
                 obj.triangles = [obj.triangles triangle_(obj.vertices(obj.indices(i,1),:),obj.vertices(obj.indices(i,2),:),obj.vertices(obj.indices(i,3),:),obj.materials(i))]; % obj.normals(i,:),
@@ -42,7 +42,7 @@ classdef mesh_ < handle
             hold off
             view(-135,30)
             rotate3d on            
-            axis([-1 1 -1 1 -1 1]*scale)
+            axis([this.position(1) this.position(1) this.position(2) this.position(2) this.position(3) this.position(3)]+[-1 1 -1 1 -1 1]*scale)
             axis vis3d
             xlabel('x [m]')
             ylabel('y [m]')
